b227deb2cdaf2bedaf905912b74fc0a8dca52520
Yamilett Estrada <36552200+yyestrada-re@users.noreply.github.com>
2019-02-04 21:56:22 -0600

    added turnToAngle method to EncoderTest

--- src/main/java/frc/robot/subsystems/EncoderTest.java
+++ src/main/java/frc/robot/subsystems/EncoderTest.java
@@ -22,13 +22,19 @@ public class EncoderTest extends Subsystem {
         return (double)talon.getSelectedSensorPosition();    
     }
 
-    public void goStraight (int distance) {
+    public void goStraight (double distance) { //where 15.24 is the circumference of the wheels and distance is in cm
         Robot.tankDriveSubsystem.driveWithConstant(); //Drives with 0.4 speed
-        double rotations = distance / (15.24 * Math.PI); //gets the number of times the wheel needs to rotate to reach a specified distance
+        double rotations = distance / (15.24 * Math.PI); //gets the number of times the wheel needs to rotate to reach a specified distance (cm) (rotations = distance/circumference)
         double units = 400 * rotations; // gets the total number of raw sensor units correlated with specified distance
         if (getPosition() >= units) { //determines if the raw sensor units thus far equates to that of specified distance. if so, stop moving
             Robot.tankDriveSubsystem.stop(); //stops all motors
         } 
+    }
+
+    public void turnToAngle(int angleOfTurn) { /* new edits */
+        double circumferenceOfRobot = 57.76; //units are in centimeter
+        double distance = (double)(circumferenceOfRobot * (angleOfTurn/360)); //gets the distance needed to turn
+        goStraight(distance);
     }
 
     @Override
