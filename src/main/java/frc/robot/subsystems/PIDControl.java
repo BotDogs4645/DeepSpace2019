1c8cad028958484a29bf2159729348e9ca0f5e4f
larguello365 <larguello365@gmail.com>
2019-02-06 18:36:42 -0600

    update

--- src/main/java/frc/robot/subsystems/PIDControl.java
+++ src/main/java/frc/robot/subsystems/PIDControl.java
@@ -36,6 +36,7 @@ public class PIDControl extends PIDSubsystem {
     talon.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
     talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
     setSetpoint(distance);      // Sets where the PID controller should move the system
+    setPercentTolerance(5);
     enable(); // Enables the PID controller.
   }
 
@@ -49,12 +50,20 @@ public class PIDControl extends PIDSubsystem {
     return (double)talon.getSelectedSensorPosition();    
   }
 
-  public void goStraight(double distance) { //where 15.24 is the circumference of the wheels and distance is in in
-    double rotations = distance / (6 * Math.PI); //gets the number of times the wheel needs to rotate to reach a specified distance (in) (rotations = distance/circumference)
+  public void goStraight() { //where 6 is the diameter of the wheels and distance is in in
+    double i = 0;
+    double d = 0;
+    double lastError = 0;
+    double rotations = getSetpoint() / (6 * Math.PI); //gets the number of times the wheel needs to rotate to reach a specified distance (in) (rotations = distance/circumference)
     double units = 400 * rotations; // gets the total number of raw sensor units correlated with specified distance
     Robot.tankDriveSubsystem.driveForward(); //Drives with 0.5 speed
-    if (getPosition() >= units) //determines if the raw sensor units thus far equates to that of specified distance. if so, stop moving
-        Robot.tankDriveSubsystem.stop(); //stops all motors
+    double p = units - getPosition(); //Represents p, how far we are from the setPosition
+    i += p; // Represents the sum of all error values
+    d = p - lastError;
+    lastError = p;
+    /*if (getPosition() >= units) //determines if the raw sensor units thus far equates to that of specified distance. if so, stop moving
+        Robot.tankDriveSubsystem.stop(); //stops all motors */
+        
   }
 
   public void turnToAngle(double distance, double angle) { //where 15.24 is the circumference of the wheels and distance is in in
