73f562a7c9cb00741b4c463d754544e67e78b036
Remyl123 <34725455+Remyl123@users.noreply.github.com>
2019-02-12 17:03:21 -0600

    made intake motor subsystem

--- src/main/java/frc/robot/commands/MoveMotorForward.java
+++ src/main/java/frc/robot/commands/MoveMotorForward.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,24 +10,28 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class MoveMotorForward extends Command {
+  public MoveMotorForward() {
+    
+      // Use requires() here to declare subsystem dependencies
+      requires(Robot.motorSub);
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    // eg. requires(chassis);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    
   }
 
   // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
+  //@Override
+  protected void execute(){
+    Robot.motorSub.motorForward();
+
   }
+
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
@@ -44,5 +48,6 @@ public class ExampleCommand extends Command {
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    Robot.motorSub.motorStop();
   }
 }
